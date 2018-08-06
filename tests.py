e6d10470b11a0cd56895a147d5cf150431a17f46
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2018-08-06 22:50:57 +0200

    added unit test class with one test

---
+++ tests.py
@@ -0,0 +1,20 @@
+import unittest
+
+import main
+
+
+class UrlShortenerTests(unittest.TestCase):
+
+    def test_shortened_length(self):
+        urla = 'http://www.google.com'
+        urlb = 'https://en.wikipedia.org/wiki/2018_FIFA_World_Cup/'
+        
+        for _ in range(5):
+            short_a = main.shorten(urla)
+            short_b = main.shorten(urlb)
+
+            self.assertEqual(12, len(short_a))
+            self.assertEqual(12, len(short_b))
+
+if __name__ == '__main__':
+    unittest.main()
